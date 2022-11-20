764faa0153e684b3be9e74939a2806f6f47452c1
adhillon0817 <avleendhillon@gmail.com>
2022-11-19 23:41:22 -0800

    added code with instructors demo and entered basic set up of html

---
+++ Main/assets/script.js
@@ -0,0 +1,64 @@
+// Declare score/time
+var countDown = 5
+
+// array of questions
+var questions = [
+// each questions is a object
+    {
+        question:
+        choices:
+        correct:
+    },
+
+    {
+        question:
+        choices:
+        correct:
+    },
+
+    {
+        question:
+        choices:
+        correct:
+    },
+
+    {
+        question:
+        choices:
+        correct:
+    },
+
+    {
+        question:
+        choices:
+        correct:
+    }, 
+
+]
+
+// DECLARE  timerEL  -('#whatever the id is in html)
+var timerEl = document.querySelector('#timer');
+
+//function to start the quiz 
+
+    //hide the start screen
+
+    // set value of countdown
+
+    // Display first question
+
+    // timer starts
+
+
+
+// function to click to answer question
+
+    //check if selected is correct or not
+
+    //if wrong
+
+        //timer goes down
+
+//Display next question
+
+// function  displayCurrentQuestion DAY 2
