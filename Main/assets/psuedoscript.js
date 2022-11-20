bdc973601700aa279090c105798814c057a37be3
adhillon0817 <avleendhillon@gmail.com>
2022-11-20 12:59:46 -0800

    added a questions array to begin with

---
+++ Main/assets/psuedoscript.js
@@ -0,0 +1,96 @@
+// // Declare score/time
+// var countDown = 5
+
+// var questionPosition = 0;
+// // array of questions
+// var questions = [
+// // each questions is a object
+//     {
+//         question:
+//         answers: 
+//         correct:
+//     },
+
+//     {
+//         question:
+//         answers:
+//         correct:
+//     },
+
+//     {
+//         question:
+//         answers:
+//         correct:
+//     },
+
+//     {
+//         question:
+//         answers:
+//         correct:
+//     },
+
+//     {
+//         question:
+//         answers:
+//         correct:
+//     }, 
+
+// ]
+
+
+// var currentQuestion = questions[questionPosition];
+
+// // for ( var i=0; i < questions.length )
+// // DECLARE  timerEL  -('#whatever the id is in html)
+// var timerEl = document.querySelector('#timer');
+
+// //function to start the quiz 
+
+
+//     //hide the start screen
+
+
+//     // set value of countdown
+
+//     // (use question position so set to 0)
+//     displayCurrentQuestion()
+
+//     // Display current question 
+
+
+//     // timer starts function startGame
+        // if countdown === 0 then 'endGame()'
+
+
+
+// // function to click to answer question
+// var currentQuestion = questions[questionPosition];
+
+
+//     //check if selected is correct or not
+
+//       //if wrong
+
+//         //timer goes down
+
+// //Display next question
+
+// // questionPosition++ by 1
+
+//  IF I passed the last question 
+        //THEN endGame
+        //Else
+
+// // displayCurrentPosition
+// displayCurrentQuestion()
+// // function  displayCurrentQuestion DAY 2
+// function displayCurrentQuestion() {
+//     and append
+// }
+// var currentQuestion = questions[questionPosition];
+
+
+
+// function endGame
+
+    // 
--- Main/assets/script.js
+++ Main/assets/script.js
@@ -1,64 +1,69 @@
-// Declare score/time
-var countDown = 5
 
-// array of questions
+//Set the time countDown
+var countDown = 59;
+
+//Creating the questions with the question, choices, and correct answer / each question is an object
 var questions = [
-// each questions is a object
     {
-        question:
-        choices:
-        correct:
+        question: "What is five plus five?",
+        options: ["ten", "one hundred", "six"],
+        answer: "ten"
     },
 
     {
-        question:
-        choices:
-        correct:
+        question: "What is six times thirty-six?",
+        options: ["seventy-two", "ninty-nine", "two-hundered sixteen"],
+        answer: "two-hundred sixteen"
     },
 
     {
-        question:
-        choices:
-        correct:
+        question: "What is seven plus eight?",
+        options: ["twenty", "fifteen", "Oats"],
+        answer: "fifteen"
     },
 
     {
-        question:
-        choices:
-        correct:
+        question: "What goes in coffee?",
+        options: ["Eggs", "Salt", "Sugar"],
+        answer: "Sugar"
     },
 
     {
-        question:
-        choices:
-        correct:
-    }, 
+        question: "What is five hundred plus one?",
+        options: ["five hundred one", "Oreo", "chocolate"],
+        answer: "five hundred one"
+    },
+
+    {
+        question: "What is ninty-nine minus three?",
+        options: ["ninty-six", "seven", "ninty-one"],
+        answer: "ninty-six"
+    },
+
+    {
+        question: "What goes in brownies?",
+        options: ["Chocolate", "Strawberry", "Vanilla"],
+        answer: "Chocolate"
+    },
+
+    {
+        question: "If you multiply a number by a zero what happens?",
+        options: ["get a zero", "returns the same number", "gets you a negative number"],
+        answer: "get a zero"
+    },
+
+    {
+        question: "Brownies are what color?",
+        options: ["Brown", "White", "Pink"],
+        answer: "Brown"
+    },
+
+    {
+        question: "Which is zero plus three hundred?",
+        options: ["ninty-nine", "three hundred", "seven"],
+        answer: "three hundred"
+    },
+
+
 
 ]
-
-// DECLARE  timerEL  -('#whatever the id is in html)
-var timerEl = document.querySelector('#timer');
-
-//function to start the quiz 
-
-    //hide the start screen
-
-    // set value of countdown
-
-    // Display first question
-
-    // timer starts
-
-
-
-// function to click to answer question
-
-    //check if selected is correct or not
-
-    //if wrong
-
-        //timer goes down
-
-//Display next question
-
-// function  displayCurrentQuestion DAY 2
